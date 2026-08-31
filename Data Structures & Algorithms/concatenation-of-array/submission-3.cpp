572560bc6d6dbcc1f0de0b6e317386511add3369
Dhanraj Kumar <dhanrajk046@gmail.com>
2026-08-31 23:59:59 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        // Method - 1 Iteration (Two Pass)
+        // vector<int> ans;
+        // {
+        //     for(int i =0; i < 2; i++)
+        //     {
+        //         for(int num:nums)
+        //         {
+        //             ans.push_back(num);
+        //         }
+        //     }
+        //     return ans;
+        // }
+
+        // Method 2 - Iteration (One Pass)
+        // int n =nums.size();
+        // vector<int> ans(2*n);
+        // for(int i=0; i<n; i++){
+        //     ans[i] = ans[i+n] = nums[i];
+        // } return ans;
+
+
+    //    Method 1 - Practice
+    //     vector<int> ans;
+    //     for(int i=0; i<2; i++){
+    //         for(int num:nums){
+    //             ans.push_back(num);
+    //         }
+    //     }
+    //     return ans;
+
+    //    Method 2 - Practice
+    int n = nums.size();
+    vector<int> ans(2*n);
+    for(int i=0; i<n;i++)
+    {
+        ans[i] = ans[i+n] = nums[i];
+    }
+    return ans;
+    }
+};
